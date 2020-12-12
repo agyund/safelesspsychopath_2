1686afd688f099058185da010813902d83e53219
agyund <62709725+agyund@users.noreply.github.com>
2020-12-13 00:57:18 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,193 @@
+$(document).ready(function () {
+
+    $("html").on("click", function () {
+        $(".crowd")[0].play();
+    })
+
+    $(".m1").on("mousedown", function () {
+        $("html").css("background-color", "black")
+        $(".title").css("fill", "red")
+        $(".hover1").css("display", "block")
+        $(".m1").css("display", "none")
+        $(".crowd")[0].pause();
+        $(".stone")[0].play();
+
+    })
+    $(".hover1").on("mouseup", function () {
+        $("html").css("background-color", "white")
+        $(".title").css("fill", "black")
+        $(".m1").css("display", "block")
+        $(".hover1").css("display", "none")
+        $(".stone")[0].pause();
+    })
+
+    $(".w1").on("mousedown", function () {
+        $("html").css("background-color", "black")
+        $(".title").css("fill", "red")
+        $(".hover2").css("display", "block")
+        $(".w1").css("display", "none")
+        $(".crowd")[0].pause();
+        $(".stone")[0].play();
+
+    })
+    $(".hover2").on("mouseup", function () {
+        $("html").css("background-color", "white")
+        $(".title").css("fill", "black")
+        $(".w1").css("display", "block")
+        $(".hover2").css("display", "none")
+        $(".stone")[0].pause();
+    })
+
+    $(".m2").on("mousedown", function () {
+        $("html").css("background-color", "black")
+        $(".title").css("fill", "red")
+        $(".hover3").css("display", "block")
+        $(".m2").css("display", "none")
+        $(".crowd")[0].pause();
+        $(".stone")[0].play();
+
+    })
+    $(".hover3").on("mouseup", function () {
+        $("html").css("background-color", "white")
+        $(".title").css("fill", "black")
+        $(".m2").css("display", "block")
+        $(".hover3").css("display", "none")
+        $(".stone")[0].pause();
+    })
+
+    $(".w2").on("mousedown", function () {
+        $("html").css("background-color", "black")
+        $(".title").css("fill", "red")
+        $(".hover4").css("display", "block")
+        $(".w2").css("display", "none")
+        $(".crowd")[0].pause();
+        $(".stone")[0].play();
+
+    })
+    $(".hover4").on("mouseup", function () {
+        $("html").css("background-color", "white")
+        $(".title").css("fill", "black")
+        $(".w2").css("display", "block")
+        $(".hover4").css("display", "none")
+        $(".stone")[0].pause();
+    })
+
+    $(".w3").on("mousedown", function () {
+        $("html").css("background-color", "black")
+        $(".title").css("fill", "red")
+        $(".hover5").css("display", "block")
+        $(".w3").css("display", "none")
+        $(".crowd")[0].pause();
+        $(".stone")[0].play();
+
+    })
+    $(".hover5").on("mouseup", function () {
+        $("html").css("background-color", "white")
+        $(".title").css("fill", "black")
+        $(".w3").css("display", "block")
+        $(".hover5").css("display", "none")
+        $(".stone")[0].pause();
+    })
+
+    $(".m3").on("mousedown", function () {
+        $("html").css("background-color", "black")
+        $(".title").css("fill", "red")
+        $(".hover6").css("display", "block")
+        $(".m3").css("display", "none")
+        $(".crowd")[0].pause();
+        $(".stone")[0].play();
+
+    })
+    $(".hover6").on("mouseup", function () {
+        $("html").css("background-color", "white")
+        $(".title").css("fill", "black")
+        $(".m3").css("display", "block")
+        $(".hover6").css("display", "none")
+        $(".stone")[0].pause();
+    })
+
+    $(".m4").on("mousedown", function () {
+        $("html").css("background-color", "black")
+        $(".title").css("fill", "red")
+        $(".hover7").css("display", "block")
+        $(".m4").css("display", "none")
+        $(".crowd")[0].pause();
+        $(".stone")[0].play();
+
+    })
+    $(".hover7").on("mouseup", function () {
+        $("html").css("background-color", "white")
+        $(".title").css("fill", "black")
+        $(".m4").css("display", "block")
+        $(".hover7").css("display", "none")
+        $(".stone")[0].pause();
+    })
+
+    $(".w4").on("mousedown", function () {
+        $("html").css("background-color", "black")
+        $(".title").css("fill", "red")
+        $(".hover8").css("display", "block")
+        $(".w4").css("display", "none")
+        $(".crowd")[0].pause();
+        $(".stone")[0].play();
+
+    })
+    $(".hover8").on("mouseup", function () {
+        $("html").css("background-color", "white")
+        $(".title").css("fill", "black")
+        $(".w4").css("display", "block")
+        $(".hover8").css("display", "none")
+        $(".stone")[0].pause();
+    })
+
+    $(".w5").on("mousedown", function () {
+        $("html").css("background-color", "black")
+        $(".title").css("fill", "red")
+        $(".hover9").css("display", "block")
+        $(".w5").css("display", "none")
+        $(".crowd")[0].pause();
+        $(".stone")[0].play();
+
+    })
+    $(".hover9").on("mouseup", function () {
+        $("html").css("background-color", "white")
+        $(".title").css("fill", "black")
+        $(".w5").css("display", "block")
+        $(".hover9").css("display", "none")
+        $(".stone")[0].pause();
+    })
+
+    $(".m5").on("mousedown", function () {
+        $("html").css("background-color", "black")
+        $(".title").css("fill", "red")
+        $(".hover10").css("display", "block")
+        $(".m5").css("display", "none")
+        $(".crowd")[0].pause();
+        $(".stone")[0].play();
+
+    })
+    $(".hover10").on("mouseup", function () {
+        $("html").css("background-color", "white")
+        $(".title").css("fill", "black")
+        $(".m5").css("display", "block")
+        $(".hover10").css("display", "none")
+        $(".stone")[0].pause();
+    })
+
+    $(".m6").on("mousedown", function () {
+        $("html").css("background-color", "black")
+        $(".title").css("fill", "red")
+        $(".hover11").css("display", "block")
+        $(".m6").css("display", "none")
+        $(".crowd")[0].pause();
+        $(".stone")[0].play();
+
+    })
+    $(".hover11").on("mouseup", function () {
+        $("html").css("background-color", "white")
+        $(".title").css("fill", "black")
+        $(".m6").css("display", "block")
+        $(".hover11").css("display", "none")
+        $(".stone")[0].pause();
+    })
+})
